7d87c156c1b9c56ad9a8065184952a730d9c3192
Sangamg145 <55407499+Sangamg145@users.noreply.github.com>
2023-07-01 16:57:27 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,37 +1,75 @@
 const router = require("express").Router();
-const Products = require("../models/products");
-
+const { Products, ProductList } = require("../models/products");
 router.get("/products", async (req, res) => {
   try {
     const products = await Products.find();
-    res
-      .status(200)
-      .json({ success: "true", status_code: 200, message: "Ok", data: products });
-  } catch (error) {
-    res.status(500).json(error);
-    console.log('first',error)
-  }
-});
-router.post("/products", async (req, res) => {
-  try {
-    const newProducts = new Products({
-      category: [{name:req.body.category[0].name,desc: req.body.category[0].desc,
-        img: req.body.category[0].img,}],
-        items: [{name:req.body.items[0].name,desc: req.body.items[0].desc,
-            img: req.body.items[0].img,}]
-    });
-    const saveUser = await newProducts.save();
-    res
-      .status(201)
+    res.status(200)
       .json({
         success: "true",
         status_code: 200,
         message: "Ok",
-        data: saveUser,
+        data: products,
       });
   } catch (error) {
     res.status(500).json(error);
   }
 });
+
+router.get("/product-list", async (req, res) => {
+    try {
+      const productList = await ProductList.find();
+      res.status(200)
+        .json({
+          success: "true",
+          status_code: 200,
+          message: "Ok",
+          data: productList,
+        });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+router.post("/products", async (req, res) => {
+  try {
+    const newProducts = new Products({
+      name: req.body.name,
+      desc: req.body.desc,
+      img: req.body.img,
+      size: req.body.size,
+      color: req.body.color,
+      liked: req.body.liked,
+    });
+    const saveUser = await newProducts.save();
+    res.status(201).json({
+      success: "true",
+      status_code: 200,
+      message: "Ok",
+      data: saveUser,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/product-list", async (req, res) => {
+    try {
+      const newProductList = new Products({
+        name: req.body.name,
+        desc: req.body.desc,
+        img: req.body.img,
+        category: req.body.category
+      });
+      const saveUser = await newProductList.save();
+      res.status(201).json({
+        success: "true",
+        status_code: 200,
+        message: "Ok",
+        data: saveUser,
+      });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
 
 module.exports = router;
