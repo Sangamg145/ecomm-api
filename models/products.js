dbd2d23cebb332d17643f03f70a71a9e1ab76674
Sangamg145 <55407499+Sangamg145@users.noreply.github.com>
2023-07-01 16:55:55 +0530

    Update products.js

--- models/products.js
+++ models/products.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose')
 let productsSchema = new mongoose.Schema({
-    category:{type:Array},
-    items:{type:Array}
+    name:{type:String,required:true},
+    desc:{type:String,required:true},
+    img:{type:String,required:true},
+    size:{type:Array,required:true},
+    color:{type:Array,required:true},
+    liked:{type:Boolean,default:false}
 },
 {timestamps:false},
 { collection : 'products',versionKey: false })
-module.exports = mongoose.model('products',productsSchema)
+
+let productListSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    desc:{type:String,required:true},
+    img:{type:String,required:true},
+    category:{type:Array,required:true},
+},
+{timestamps:false},
+{ collection : 'productList',versionKey: false })
+const Products = mongoose.model('products',productsSchema)
+const ProductList = mongoose.model('productList',productListSchema)
+module.exports = {Products,ProductList}
